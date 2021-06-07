8e4ced0b337c3143c99325350b7c2fa407f99563
tatianakharitonovich <k.tanya@tut.by>
2021-06-07 23:28:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let burger = document.querySelector('.header__link_img_ip');
 console.log(burger);
-let nav = document.querySelectorAll('.header__link');
-console.log(nav);
-let blockNav = document.querySelector('.header__nav_general');
+let burgerClose = document.querySelector('.header__link_imgclose');
+let navLink = document.querySelectorAll('.header__link');
+console.log(navLink);
+let blockNav = document.querySelector('.header__blocknav');
 console.log(blockNav);
 
 burger.addEventListener('click', ()=> {
 	burger.classList.toggle ('header__link_img_click'); 
-	nav.forEach (item => item.classList.toggle ('header__link_click'));
+	navLink.forEach (item => item.classList.toggle ('header__link_click'));
 	blockNav.classList.toggle ('header__blocknav_click');
+	burgerClose.classList.toggle('header__link_imgclose_click');
 });
 
-nav.forEach (item => {
-	item.addEventListener('click', ()=> {
-		nav.forEach (item => item.classList.remove ('header__link_click'));
+function close() {
+		navLink.forEach (item => item.classList.remove ('header__link_click'));
 		burger.classList.remove ('header__link_img_click');
 		blockNav.classList.remove ('header__blocknav_click');
-		})
-});
+		burgerClose.classList.remove('header__link_imgclose_click');
+};
+
+burgerClose.addEventListener('click',close);
+
+navLink.forEach (item => {item.addEventListener('click', close)});
+
+
+
 
 
