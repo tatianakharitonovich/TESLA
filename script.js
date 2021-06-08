6c1c89e60fb087b1e47695f24eb85ed56b5e71f7
tatianakharitonovich <k.tanya@tut.by>
2021-06-08 11:03:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let burger = document.querySelector('.header__link_img_ip');
 console.log(burger);
+
 let burgerClose = document.querySelector('.header__link_imgclose');
+
 let navLink = document.querySelectorAll('.header__link');
 console.log(navLink);
+
 let blockNav = document.querySelector('.header__blocknav');
 console.log(blockNav);
 
@@ -10,14 +13,14 @@ burger.addEventListener('click', ()=> {
 	burger.classList.toggle ('header__link_img_click'); 
 	navLink.forEach (item => item.classList.toggle ('header__link_click'));
 	blockNav.classList.toggle ('header__blocknav_click');
-	burgerClose.classList.toggle('header__link_imgclose_click');
+	burgerClose.classList.toggle ('header__link_imgclose_click');
 });
 
 function close() {
 		navLink.forEach (item => item.classList.remove ('header__link_click'));
 		burger.classList.remove ('header__link_img_click');
 		blockNav.classList.remove ('header__blocknav_click');
-		burgerClose.classList.remove('header__link_imgclose_click');
+		burgerClose.classList.remove ('header__link_imgclose_click');
 };
 
 burgerClose.addEventListener('click',close);
